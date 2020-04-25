68196bbe437cde1cbc41f7390ccc3fb582a3edf0
Ubuntu <ubuntu@ip-172-31-87-108.ec2.internal>
2020-04-25 06:13:15 +0000

    0.1

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _7f49c8d4 = () => interopDefault(import('../pages/send.vue' /* webpackChunkName: "pages/send" */))
 const _3ad62e36 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
@@ -22,6 +23,10 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/send",
+    component: _7f49c8d4,
+    name: "send"
+  }, {
     path: "/",
     component: _3ad62e36,
     name: "index"
---
+++ pages/send.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+
+// const Twit = require('twit')
+
+import Twitter from 'twitter-lite';
+
+const client = new Twitter({
+  consumer_key:         '10J4YciYNsuDXm5ru981rl3ct',
+  consumer_secret:      '63X9MCoG0uulsAfXUWsxigeTCGo5IKsBu3OzYFNQD4wpkvvkXg',
+  access_token:         '111849491-bTOTXWKjilZ9CEM4zU5lgu66Lo2kYf9jWqrf4UPO',
+  access_token_secret:  '1YiGXwtnAzzcRVrbcRnaJCiVtc1sKuEGPPB33KQQeLGAb'
+})
+
+//
+//  tweet 'hello world!'
+//
+
+
+
+export default {
+  data() {
+    return {
+      signedIn: false
+    }
+  },
+  methods: {
+
+    sendMessage() {
+        
+        
+        this.client
+          .get("account/verify_credentials")
+          .then(results => {
+            console.log("results", results);
+          })
+          .catch(console.error);
+        
+            // this.T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+            //   console.log(data);
+            //   alert(data);
+            // })
+    }
+
+  },
+  created() {
+      
+      this.sendMessage();
+
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.logo {
+  margin-bottom: 30px;
+  max-width: 400px;
+}
+</style>
